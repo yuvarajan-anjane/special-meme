6579b4b3a29d23c0544a115d1da653b0def509b4
Yuvarajan S <yuvarajan.sundararajan@anjanetech.com>
2022-03-24 07:35:35 +0000

    till_src_fresh

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,12 +1,7 @@
-with customers as (
-
-    select
+select
         id as customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ source('jaffle_shop','customers') }}
 
-)
-
-select * from customers
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,13 +1,7 @@
-with orders as (
-
-    select
+select
         id as order_id,
         user_id as customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
-
-)
-
-select * from orders
+    from {{ source('jaffle_shop','orders') }}
