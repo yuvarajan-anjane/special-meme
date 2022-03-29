a84deb3d8d0605186fa30a243f8cbdd05b6f8f51
Yuvarajan S <yuvarajan.sundararajan@anjanetech.com>
2022-03-29 09:15:14 +0000

    test in assert paymentss

---
+++ tests/assert_stg_payments_total_value_positive.sql
@@ -0,0 +1,19 @@
+with 
+payments as (
+    select * from{{ref('stg_payments')}}
+)
+
+select
+    order_id,
+    sum(amount) as total_amount
+from payments
+group by order_id
+having total_amount < 0
+
+
+/*select
+    order_id,
+    sum(amount) as total_amount
+from {{ ref('stg_payments') }}
+group by 1
+having not(total_amount >= 0)*/
